1d7ee44d78c34cd95d547dc72846d7f7ce65e1d9
mhmdraiss <muhraies11@gmail.com>
2024-08-28 22:16:37 +0700

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -3,4 +3,7 @@ import streamlit as st
 def app():
     st.title('Credit Card Approval Prediction')
     st.header('Created By:')
-    st.header('Team Eleven for Iconic IT 2024')
+    st.header('Team Eleven')
+    st.subheader('For: /n Iconic IT 2024')
+    
+    
